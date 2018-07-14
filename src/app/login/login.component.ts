8b8597a9ca8011b231becf55044df13ead5f5092
Dhruv <dhruvdev7794@gmail.com>
2018-07-15 00:43:27 +0530

    sample

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,4 +13,20 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSignIn(googleUser) {
+    const profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  }
+  signOut() {
+    console.log('User not signed out.');
+    // const auth2 = gapi.auth2.getAuthInstance();
+    // auth2.signOut().then(function () {
+    //   console.log('User signed out.');
+    // });
+  }
+
+
 }
