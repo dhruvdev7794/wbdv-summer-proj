572489cf3cb9d2e922cd695ca2ec861e4793b879
Dhruv <dhruvdev7794@gmail.com>
2018-07-15 18:22:47 +0530

    changed app name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/wbdv-proj-angular'));
+app.use(express.static(__dirname + '/dist/wbdv-summer-proj1'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/wbdv-proj-angular/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/wbdv-summer-proj1/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
