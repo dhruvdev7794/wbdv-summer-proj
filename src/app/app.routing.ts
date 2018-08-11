49d8b236354936524305410e663e430e1de950bd
jessica <gonzalez.je@husky.neu.edu>
2018-08-11 00:12:24 -0400

    use service

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,12 @@ import {Routes, RouterModule} from '@angular/router';
 import {ProjListComponent} from './proj-list/proj-list.component';
 import {ImageListComponent} from './image-list/image-list.component';
 import {ImageDispComponent} from './image-disp/image-disp.component';
+import {LoginComponent} from './login/login.component';
 import {ImageModificationsComponent} from './image-modifications/image-modifications.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: ProjListComponent},
+  {path: 'home', component: LoginComponent},
   {path: 'project/:id/image', component: ImageListComponent},
   {path: 'image/:imageId/edit', component: ImageModificationsComponent}
 ];
